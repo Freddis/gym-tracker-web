a394fb03bbba192c31973a2e00181899914bbfc3
Alex Sarychev <freddis336@gmail.com>
2025-05-26 18:46:08 +0300

    Profile Dropdown Menu

--- src/frontend/components/layout/Header/Header.tsx
+++ src/frontend/components/layout/Header/Header.tsx
@@ -1,35 +1,24 @@
-import {Link, useNavigate} from '@tanstack/react-router';
-import {FC, MouseEventHandler, ReactNode, useContext} from 'react';
+import {Link} from '@tanstack/react-router';
+import {FC, ReactNode, useContext} from 'react';
 import {AuthContext} from '../AuthProvider/AuthContext';
-import {AppLink} from '../../atoms/AppLink/AppLink';
 import {HeaderLink} from './components/HeaderLink';
 import {AppLogo} from '../../atoms/AppLogo/AppLogo';
 import {EditThemeContext} from '../ThemeProvider/context/EditThemeContext';
 import {Theme} from '../ThemeProvider/enums/Theme';
 import {ThemeContext} from '../ThemeProvider/context/ThemeContext';
 import {useAppPartialTranslation} from '../../../i18n/useAppPartialTranslation';
-import {DarkModeSwitch} from '../../composite/DarkModeSwitch/DarkModeSwitch';
+import {ProfileDropdownMenu} from './components/ProfileDropdownMenu/ProfileDropdownMenu';
+import {DarkModeSwitch} from './components/DarkModeSwitch/DarkModeSwitch';
 
-function exists<T>(x: T| null | undefined): x is T {
-  return !!x;
-}
-export const Conditional: FC<{condition: boolean, children: ReactNode}> & {exists: typeof exists} = (props) => {
+export const Conditional: FC<{condition: boolean, children: ReactNode}> = (props) => {
   return props.condition ? props.children : null;
 };
-Conditional.exists = exists;
 
 export const Header: FC = () => {
   const auth = useContext(AuthContext);
-  const navigate = useNavigate();
   const {t, i18n} = useAppPartialTranslation((x) => x.layout.header);
   const theme = useContext(ThemeContext);
   const themeContext = useContext(EditThemeContext);
-
-  const logout: MouseEventHandler<HTMLAnchorElement> = (e) => {
-    e.preventDefault();
-    auth.logout();
-    navigate({to: '/'});
-  };
   const toggleTheme = () => {
     themeContext.setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
   };
@@ -69,13 +58,12 @@ export const Header: FC = () => {
           <Conditional condition={!auth.user}>
             <HeaderLink to="/auth/login">{t(i18n.menu.signIn)}</HeaderLink>
           </Conditional>
-          <DarkModeSwitch onClick={toggleTheme} checked={theme === Theme.Dark} />
         </div>
         <Conditional condition={!!auth.user}>
-          <div className="flex items-center ml-20">
-            <span className="text-base">{auth.user?.name} (<AppLink onClick={logout}>{t(i18n.menu.signOut)}</AppLink>)</span>
-            <img src="/images/avatar.gif" className="ml-2 border-light border-2 shadow-2xl shadow-red-600 rounded-full w-12"/>
-          </div>
+          <ProfileDropdownMenu />
+        </Conditional>
+        <Conditional condition={!auth.user}>
+          <DarkModeSwitch onClick={toggleTheme} checked={theme === Theme.Dark} />
         </Conditional>
       </div>
     </div>
---
+++ src/frontend/components/layout/Header/components/ProfileDropdownMenu/ProfileDropdownMenu.tsx
@@ -0,0 +1,62 @@
+import {useNavigate} from '@tanstack/react-router';
+import {FC, MouseEventHandler, useContext} from 'react';
+import {AuthContext} from '../../../AuthProvider/AuthContext';
+import {useAppPartialTranslation} from '../../../../../i18n/useAppPartialTranslation';
+import {FiChevronDown} from 'react-icons/fi';
+import {
+  AppDropdownMenu,
+  AppDropdownMenuContent,
+  AppDropdownMenuItem,
+  AppDropdownMenuLabel,
+  AppDropdownMenuSeparator,
+  AppDropdownMenuTrigger,
+} from '../../../../atoms/AppDropdownMenu/AppDropdownMenu';
+import {AppSwitch} from '../../../../atoms/AppSwitch/AppSwitch';
+import {EditThemeContext} from '../../../ThemeProvider/context/EditThemeContext';
+import {ThemeContext} from '../../../ThemeProvider/context/ThemeContext';
+import {Theme} from '../../../ThemeProvider/enums/Theme';
+
+
+export const ProfileDropdownMenu: FC = () => {
+  const {t, i18n} = useAppPartialTranslation((x) => x.layout.header);
+  const auth = useContext(AuthContext);
+  const theme = useContext(ThemeContext);
+  const themeContext = useContext(EditThemeContext);
+  const navigate = useNavigate();
+  const logout: MouseEventHandler<HTMLDivElement> = (e) => {
+    e.preventDefault();
+    auth.logout();
+    navigate({to: '/'});
+  };
+  const toggleDarkTheme = () => {
+    const newTheme = theme === Theme.Dark ? Theme.Light : Theme.Dark;
+    themeContext.setTheme(newTheme);
+  };
+  return (
+    <AppDropdownMenu>
+    <AppDropdownMenuTrigger className="text-lg">
+    <div className="flex items-center cursor-pointer">
+      <span className="text-base ">{auth.user?.name}</span>
+      <FiChevronDown className=" relative" />
+      <img src="/images/avatar.gif" className="ml-2 border-light border-2 shadow-2xl shadow-red-600 rounded-full w-12"/>
+    </div>
+    </AppDropdownMenuTrigger>
+    <AppDropdownMenuContent sideOffset={-2} className="w-50">
+      <AppDropdownMenuLabel>My Profile</AppDropdownMenuLabel>
+      <AppDropdownMenuSeparator />
+      <AppDropdownMenuItem onClick={(e) => e.preventDefault()} className="focus:bg-inherit">
+        <div className="flex flex-row items-center w-full">
+          <span>Dark Theme</span>
+          <div className="flex-grow flex flex-row-reverse">
+          <AppSwitch onClick={toggleDarkTheme} checked={theme === Theme.Dark}/>
+          </div>
+        </div>
+      </AppDropdownMenuItem>
+      <AppDropdownMenuItem onClick={logout} className="cursor-pointer focus:bg-inherit focus:text-accent">
+        <span>{t(i18n.menu.signOut)}</span>
+      </AppDropdownMenuItem>
+    </AppDropdownMenuContent>
+  </AppDropdownMenu>
+
+  );
+};
