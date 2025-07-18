54ff7c3c7e90a0cf84e3634242c5ec5544490186
Alex Sarychev <freddis336@gmail.com>
2025-07-18 19:53:45 +0300

    Removing commented code

--- src/backend/drizzle/db.ts
+++ src/backend/drizzle/db.ts
@@ -1,19 +1,5 @@
-// import pg from 'pg';
-
-// import {drizzle as pgDrizzle} from 'drizzle-orm/node-postgres';
 import * as schema from './schema/schema';
 import * as relations from './schema/relations';
-// import {QueryLogger} from '../utils/QueryLogger/QueryLogger';
-// import {serverConfig} from '../utils/ServerConfig/config';
-
-// export const pgClient = new pg.Client({
-//   ...serverConfig.database,
-//   connectionTimeoutMillis: 2000,
-// });
-// export const db = pgDrizzle(pgClient, {
-//   logger: new QueryLogger(false, true, 'postgres'),
-//   schema: {...schema, ...relations},
-// });
 
 export const dbSchema = schema;
 export const dbRelations = relations;
