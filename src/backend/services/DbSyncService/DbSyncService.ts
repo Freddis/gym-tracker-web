6e12e820e5f86f061e47992a933792f3fed5a688
Alex Sarychev <freddis336@gmail.com>
2025-08-01 18:14:47 +0300

    Fixing Id sequences when syncing with prod DB

--- src/backend/services/DbSyncService/DbSyncService.ts
+++ src/backend/services/DbSyncService/DbSyncService.ts
@@ -1,6 +1,7 @@
-import {PgTable} from 'drizzle-orm/pg-core';
+import {PgColumn, PgTable} from 'drizzle-orm/pg-core';
 import {DrizzleService} from '../DrizzleService/DrizzleService';
 import {Logger} from '../../../common/utils/Logger/Logger';
+import {desc} from 'drizzle-orm';
 
 /**
  * Syncs production and local databases.
@@ -21,21 +22,21 @@ export class DbSyncService {
   async pushSync() {
     this.logger.info('Starting sync');
     const localDb = await this.local.getDb();
-    const tables: Array<[string, PgTable]> = [
-      ['users', localDb._.fullSchema.users],
-      ['exercises', localDb._.fullSchema.exercises],
-      ['exercise muscles', localDb._.fullSchema.muscles],
-      ['workouts', localDb._.fullSchema.workouts],
-      ['workout exercises', localDb._.fullSchema.workoutExercises],
-      ['workout sets', localDb._.fullSchema.workoutExerciseSets],
-      ['images', localDb._.fullSchema.images],
+    const tables: Array<[string, PgTable, PgColumn]> = [
+      ['users', localDb._.fullSchema.users, localDb._.fullSchema.users.id],
+      ['exercises', localDb._.fullSchema.exercises, localDb._.fullSchema.exercises.id],
+      ['exercise muscles', localDb._.fullSchema.muscles, localDb._.fullSchema.muscles.id],
+      ['workouts', localDb._.fullSchema.workouts, localDb._.fullSchema.workouts.id],
+      ['workout exercises', localDb._.fullSchema.workoutExercises, localDb._.fullSchema.workoutExercises.id],
+      ['workout sets', localDb._.fullSchema.workoutExerciseSets, localDb._.fullSchema.workoutExerciseSets.id],
+      ['images', localDb._.fullSchema.images, localDb._.fullSchema.images.id],
     ];
 
     for (const [name, table] of [...tables].reverse()) {
       await this.deleteTable(name, table);
     }
-    for (const [name, table] of tables) {
-      await this.syncTable(name, table);
+    for (const [name, table, id] of tables) {
+      await this.syncTable(name, table, id);
     }
     this.logger.info('Done');
   }
@@ -46,7 +47,13 @@ export class DbSyncService {
     await prodDb.delete(table);
   }
 
-  protected async syncTable(name: string, table: PgTable) {
+  protected async syncTable(name: string, table: PgTable, orderColumn: PgColumn,) {
+    const nameSmb = Symbol.for('drizzle:Name');
+    const schemaSmb = Symbol.for('drizzle:Schema');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const tableName = (table as any)[nameSmb];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const tableSchema = (table as any)[schemaSmb];
     this.logger.info(`Syncing ${name}`);
     const prodDb = await this.prod.getDb();
     const localDb = await this.local.getDb();
@@ -54,7 +61,7 @@ export class DbSyncService {
     const limit = this.chunkSize;
     let offset = 0;
     while (true) {
-      const values = await localDb.select().from(table).limit(limit).offset(offset);
+      const values = await localDb.select().from(table).orderBy(desc(orderColumn)).limit(limit).offset(offset);
       if (values.length === 0) {
         break;
       }
@@ -64,5 +71,12 @@ export class DbSyncService {
       }
       offset += limit;
     }
+    await prodDb.execute(`
+      SELECT setval(
+        pg_get_serial_sequence('${tableSchema}.${tableName}', 'id'),
+        COALESCE((SELECT MAX(id) FROM ${tableSchema}.${tableName}), 0) + 1,
+        false
+      );
+    `);
   }
 }
