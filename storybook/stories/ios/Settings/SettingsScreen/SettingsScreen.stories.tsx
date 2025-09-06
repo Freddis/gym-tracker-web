51a9f1c0140d124701e5f6d6adcefa2d9c51b2a9
Alex Sarychev <freddis336@gmail.com>
2025-09-06 19:28:14 +0300

    Mobile Settings screen

---
+++ storybook/stories/ios/Settings/SettingsScreen/SettingsScreen.stories.tsx
@@ -0,0 +1,32 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {Color} from '../../../../../src/frontend/utils/design-system/types/Color';
+import {StoryBookDisplay} from '../../../../components/StoryBookDisplay/StoryBookDisplay';
+import {SettingsScreen} from './SettingsScreen';
+
+const meta = {
+  title: 'IOS/Settings/Settings',
+  component: SettingsScreen,
+  tags: ['autodocs'],
+  globals: {
+    viewport: {value: 'tablet', isRotated: false},
+  },
+  args: {
+  },
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'Login Form',
+      },
+    },
+  },
+  decorators: [(Story) => <StoryBookDisplay story={<Story/>} palette={Color.Neutral} column />],
+} satisfies Meta<typeof SettingsScreen>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+
+};
+
---
+++ storybook/stories/ios/Settings/SettingsScreen/SettingsScreen.tsx
@@ -0,0 +1,35 @@
+import {FC} from 'react';
+import {IphoneDisplay} from '../../../../components/IphoneDisplay/IphoneDisplay';
+import {MobileScreenContainer} from '../../../../components/MobileScreenContainer/MobileScreenContainer';
+import {AppSwitch} from '../../../../../src/frontend/components/atoms/AppSwitch/AppSwitch';
+import {AppLink} from '../../../../../src/frontend/components/atoms/AppLink/AppLink';
+import {MobileBlock} from '../../../../components/MobileScreenContainer/MobileBlock/MobileBlock';
+
+export const SettingsScreen: FC = () => (
+  <IphoneDisplay tab={3}>
+   <MobileScreenContainer className="flex flex-col h-full">
+      <MobileBlock>
+      <div className="flex flex-col">
+        <div className="flex">
+          <div className="grow">Name:</div>
+          <div>Alex Sarychev</div>
+        </div>
+        <div className="border-b-1 border-on-surface/15 my-3" />
+        <div className="flex">
+          <div className="grow">Email:</div>
+          <div>alex@alex-sarychev.com</div>
+        </div>
+        <div className="border-b-1 border-on-surface/15 my-3" />
+        <div className="flex">
+          <div className="grow">Dark Mode:</div>
+          <div><AppSwitch checked /></div>
+        </div>
+        <div className="flex flex-col items-center gap-5 mt-10">
+          <AppLink>Re-Sync Data</AppLink>
+          <AppLink>Sign Out</AppLink>
+          </div>
+      </div>
+      </MobileBlock>
+    </MobileScreenContainer>
+  </IphoneDisplay>
+);
