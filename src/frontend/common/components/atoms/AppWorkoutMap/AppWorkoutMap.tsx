004633ffa4171a713ef770ecc8ac3fdcb3559378
Alex Sarychev <freddis336@gmail.com>
2026-04-30 20:45:03 +0300

    Google map follows theme

--- src/frontend/common/components/atoms/AppWorkoutMap/AppWorkoutMap.tsx
+++ src/frontend/common/components/atoms/AppWorkoutMap/AppWorkoutMap.tsx
@@ -1,12 +1,15 @@
-import {Polyline, Map} from '@vis.gl/react-google-maps';
-import {FC, useMemo} from 'react';
+import {Polyline, Map, ColorScheme} from '@vis.gl/react-google-maps';
+import {FC, useContext, useMemo} from 'react';
 import {getHeatColor} from '../../../../website/utils/getHeatColor';
 import {ProcessedPathData} from '../../../../website/utils/usePathDataProcessing';
+import {ThemeContext} from '../../layout/ThemeProvider/context/ThemeContext';
+import {Theme} from '../../layout/ThemeProvider/enums/Theme';
 
 export interface AppWorkoutMapProps {
   data: ProcessedPathData;
 }
 export const AppWorkoutMap: FC<AppWorkoutMapProps> = (props) => {
+  const theme = useContext(ThemeContext);
   const lines = useMemo(() => props.data.chunks.map((chunk, i) => {
     const {minSpeed, maxSpeed} = props.data;
     const speed = chunk.reduce((acc, curr) => acc + (curr.speed ?? 0), 0) / chunk.length;
@@ -27,6 +30,7 @@ export const AppWorkoutMap: FC<AppWorkoutMapProps> = (props) => {
         defaultBounds={props.data.bounds}
         gestureHandling="greedy"
         disableDefaultUI
+        colorScheme={theme === Theme.Dark ? ColorScheme.DARK : ColorScheme.LIGHT}
       >
       {lines}
       </Map>
