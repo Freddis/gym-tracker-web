831ff082881546ec55c4d17bda311b5421f9bcb1
Alex Sarychev <freddis336@gmail.com>
2026-05-13 13:41:58 +0300

    Sync with prod optimization

--- src/backend/services/DbSyncService/DbSyncService.ts
+++ src/backend/services/DbSyncService/DbSyncService.ts
@@ -22,25 +22,32 @@ export class DbSyncService {
   async pushSync() {
     this.logger.info('Starting sync');
     const localDb = await this.local.getDb();
-    const tables: Array<[string, PgTable, PgColumn]> = [
-      ['managers', localDb._.fullSchema.managers, localDb._.fullSchema.managers.id],
-      ['users', localDb._.fullSchema.users, localDb._.fullSchema.users.id],
-      ['exercises', localDb._.fullSchema.exercises, localDb._.fullSchema.exercises.id],
-      ['exercise muscles', localDb._.fullSchema.muscles, localDb._.fullSchema.muscles.id],
-      ['workouts', localDb._.fullSchema.workouts, localDb._.fullSchema.workouts.id],
-      ['workout exercises', localDb._.fullSchema.workoutExercises, localDb._.fullSchema.workoutExercises.id],
-      ['workout sets', localDb._.fullSchema.workoutExerciseSets, localDb._.fullSchema.workoutExerciseSets.id],
-      ['weight', localDb._.fullSchema.weight, localDb._.fullSchema.weight.id],
-      ['images', localDb._.fullSchema.images, localDb._.fullSchema.images.id],
-      ['entries', localDb._.fullSchema.entries, localDb._.fullSchema.entries.id],
-      ['translations', localDb._.fullSchema.translations, localDb._.fullSchema.translations.id],
+    const tables: Array<[string, PgTable, PgColumn, boolean]> = [
+      ['managers', localDb._.fullSchema.managers, localDb._.fullSchema.managers.id, false],
+      ['images', localDb._.fullSchema.images, localDb._.fullSchema.images.id, false],
+      ['users', localDb._.fullSchema.users, localDb._.fullSchema.users.id, false],
+      ['food', localDb._.fullSchema.food, localDb._.fullSchema.food.id, true],
+      ['food components', localDb._.fullSchema.foodComponents, localDb._.fullSchema.foodComponents.id, false],
+      ['meals', localDb._.fullSchema.meals, localDb._.fullSchema.meals.id, false],
+      ['meal food', localDb._.fullSchema.mealFoodComponents, localDb._.fullSchema.mealFoodComponents.id, false],
+      ['outdoor runs', localDb._.fullSchema.outdoorRuns, localDb._.fullSchema.outdoorRuns.id, false],
+      ['outdoor walks', localDb._.fullSchema.outdoorWalks, localDb._.fullSchema.outdoorWalks.id, false],
+      ['calorie goals', localDb._.fullSchema.calorieGoals, localDb._.fullSchema.calorieGoals.id, false],
+      ['exercises', localDb._.fullSchema.exercises, localDb._.fullSchema.exercises.id, true],
+      ['exercise muscles', localDb._.fullSchema.muscles, localDb._.fullSchema.muscles.id, false],
+      ['workouts', localDb._.fullSchema.workouts, localDb._.fullSchema.workouts.id, false],
+      ['workout exercises', localDb._.fullSchema.workoutExercises, localDb._.fullSchema.workoutExercises.id, false],
+      ['workout sets', localDb._.fullSchema.workoutExerciseSets, localDb._.fullSchema.workoutExerciseSets.id, false],
+      ['weight', localDb._.fullSchema.weight, localDb._.fullSchema.weight.id, false],
+      ['entries', localDb._.fullSchema.entries, localDb._.fullSchema.entries.id, true],
+      ['translations', localDb._.fullSchema.translations, localDb._.fullSchema.translations.id, false],
     ];
 
     for (const [name, table] of [...tables].reverse()) {
       await this.deleteTable(name, table);
     }
-    for (const [name, table, id] of tables) {
-      await this.syncTable(name, table, id);
+    for (const [name, table, id, uuid] of tables) {
+      await this.syncTable(name, table, id, uuid);
     }
     this.logger.info('Done');
   }
@@ -51,7 +58,7 @@ export class DbSyncService {
     await prodDb.delete(table);
   }
 
-  protected async syncTable(name: string, table: PgTable, orderColumn: PgColumn,) {
+  protected async syncTable(name: string, table: PgTable, orderColumn: PgColumn, uuid: boolean) {
     const nameSmb = Symbol.for('drizzle:Name');
     const schemaSmb = Symbol.for('drizzle:Schema');
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -74,6 +81,9 @@ export class DbSyncService {
         break;
       }
       offset += limit;
+    }
+    if (uuid) {
+      return;
     }
     await prodDb.execute(`
       SELECT setval(
