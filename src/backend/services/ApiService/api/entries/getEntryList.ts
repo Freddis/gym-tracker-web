fe822646d53fef12b3f015b54abab649d782416d
Alex Sarychev <freddis336@gmail.com>
2026-06-20 13:32:13 +0300

    Fixing feed that displays records of other users

--- src/backend/services/ApiService/api/entries/getEntryList.ts
+++ src/backend/services/ApiService/api/entries/getEntryList.ts
@@ -5,6 +5,8 @@ import {PaginatedResult} from '../../types/PaginatedResult';
 import {getEntryListQueryValidator} from './validators/getEntriesQueryValidator';
 import {feedEntryValidator} from './validators/feedEntryValidator';
 import {FeedEntry} from '../../../FeedEntryService/types/FeedEntry';
+import {ApiError} from '../../errors/ApiError';
+import {ApiErrorCode} from '../../types/ApiErrorCode';
 
 export const getEntryList = RouteFactory.createRoute({
   type: ApiRouteType.Public,
@@ -18,8 +20,13 @@ export const getEntryList = RouteFactory.createRoute({
     }),
   },
   handler: async (ctx): Promise<PaginatedResult<FeedEntry>> => {
+    const userId = ctx.params.query.own ? ctx.viewer?.id : undefined;
+    if (ctx.params.query.own && !ctx.viewer) {
+      throw new ApiError(ApiErrorCode.Unauthorized);
+    }
     const result = await ctx.services.models.feedEntry.paginateForUser({
       ...ctx.params.query,
+      userId: userId ? [userId] : [],
       language: ctx.language,
       perPage: 10,
     });
